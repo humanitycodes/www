5acf72fec740eedfbd6ed5ed3162fab3d4074181
Chris Fritz <chrisvfritz@gmail.com>
2015-11-30 16:58:58 -0500

    fixed embarrassing debugging line left in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ private
   # --------------
 
   def current_user
-    @current_user ||= User.last #User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
     rescue ActiveRecord::RecordNotFound
   end
   helper_method :current_user
