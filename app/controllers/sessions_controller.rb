0e020e1297a00e85cbd00c0cf2e8a9d09410ad68
Chris Fritz <chrisvfritz@gmail.com>
2016-01-02 03:26:49 -0500

    fixed sign in redirect when not already signed into github

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,16 @@
 class SessionsController < ApplicationController
 
   def create
+    redirect_url = case request.referer
+    when 'https://github.com/' then lessons_path
+    else                            request.referer
+    end
+
     user = User.from_omniauth(request.env['omniauth.auth'])
     session[:user_id] = user.id
-    redirect_to :back, notice: 'Signed in!'
+    redirect_to redirect_url, notice: 'Signed in!'
   rescue
-    redirect_to :back, alert: 'Authentication error!'
+    redirect_to redirect_url, alert: 'Authentication error!'
   end
 
   def destroy
