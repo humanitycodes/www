4bc75041e3dc3c3d15279f7c339fff142c5b1579
Chris Fritz <chrisvfritz@gmail.com>
2016-03-01 11:17:41 -0500

    fix for rare double-requests to create a repo

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,6 +6,10 @@ class RepositoriesController < ApplicationController
       oauth_token: current_user.github_token
     ).repos.create(name: "codelab-#{params[:key]}")
     redirect_to request.referer + '#project'
+  # Will occur in rare cases where a repo creation
+  # is triggered twice in a row.
+  rescue Github::Error::UnprocessableEntity
+    redirect_to request.referer + '#project'
   end
 
 end
